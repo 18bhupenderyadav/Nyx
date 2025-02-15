cf73a66203e461bbcfbc1b49ac6692af70389af1
Bhupender Yadav <18bhupenderyadav@gmail.com>
2025-02-15 19:32:16 +0530

    Exit Command

--- src/main/java/com/nyx/shell/commands/ExitCommand.java
+++ src/main/java/com/nyx/shell/commands/ExitCommand.java
@@ -2,9 +2,12 @@ package com.nyx.shell.commands;
 
 import com.nyx.shell.Command;
 
+import static java.lang.System.exit;
+
 public class ExitCommand implements Command {
     @Override
     public void execute(String[] args) {
         System.out.print("exit 0");
+        exit(0);
     }
 }
