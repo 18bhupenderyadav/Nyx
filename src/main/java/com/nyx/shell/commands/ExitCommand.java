31d59a5a5f389f7b5d54753f083ab4e37de026e5
Bhupender Yadav <18bhupenderyadav@gmail.com>
2025-02-15 19:39:50 +0530

    Exit Command fix

--- src/main/java/com/nyx/shell/commands/ExitCommand.java
+++ src/main/java/com/nyx/shell/commands/ExitCommand.java
@@ -7,7 +7,7 @@ import static java.lang.System.exit;
 public class ExitCommand implements Command {
     @Override
     public void execute(String[] args) {
-        System.out.print("exit 0");
+//        System.out.print("exit 0");
         exit(0);
     }
 }
