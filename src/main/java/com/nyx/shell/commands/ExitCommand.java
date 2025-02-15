b8ba34ce8e31c49baf9bd61981d68e207ff74b92
Bhupender Yadav <18bhupenderyadav@gmail.com>
2025-02-15 19:43:15 +0530

    Exit Command refactor

--- src/main/java/com/nyx/shell/commands/ExitCommand.java
+++ src/main/java/com/nyx/shell/commands/ExitCommand.java
@@ -2,12 +2,29 @@ package com.nyx.shell.commands;
 
 import com.nyx.shell.Command;
 
-import static java.lang.System.exit;
-
+/**
+ * A built-in command that terminates the shell.
+ */
 public class ExitCommand implements Command {
+
+    /**
+     * Executes the exit command.
+     * If an argument is provided, it will be used as the exit code.
+     * Otherwise, the shell exits with a status code of 0.
+     *
+     * @param args The exit code as a string (optional).
+     */
     @Override
     public void execute(String[] args) {
-//        System.out.print("exit 0");
-        exit(0);
+        int exitCode = 0; // Default exit code.
+        if (args.length > 0) {
+            try {
+                exitCode = Integer.parseInt(args[0]);
+            } catch (NumberFormatException e) {
+                System.out.println("Invalid exit code. Using default exit code 1.");
+                exitCode = 1;
+            }
+        }
+        System.exit(exitCode);
     }
 }
