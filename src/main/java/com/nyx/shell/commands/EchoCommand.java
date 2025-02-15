22c051a027c17a5d6169f08052ad0e5780eb85b7
Bhupender Yadav <18bhupenderyadav@gmail.com>
2025-02-15 20:15:39 +0530

    Refactor echo command

--- src/main/java/com/nyx/shell/commands/EchoCommand.java
+++ src/main/java/com/nyx/shell/commands/EchoCommand.java
@@ -2,14 +2,19 @@ package com.nyx.shell.commands;
 
 import com.nyx.shell.Command;
 
+/**
+ * A built-in command that echoes the provided arguments to the standard output.
+ */
 public class EchoCommand implements Command {
 
+    /**
+     * Executes the echo command by joining the given arguments with spaces and printing the result.
+     *
+     * @param args the arguments to be echoed.
+     */
     @Override
     public void execute(String[] args) {
-        String output = "";
-        for (String i : args) {
-            output = output + i + " ";
-        }
-        System.out.println(output);
+        // Join the arguments with a single space and print the result.
+        System.out.println(String.join(" ", args));
     }
 }
